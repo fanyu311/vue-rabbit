d1e950ca5baf8330974de70a014b72eb6244707f
fanyu311 <116265245+fanyu311@users.noreply.github.com>
2023-06-05 12:06:37 +0200

    axios

---
+++ src/utils/http.js
@@ -0,0 +1,24 @@
+// axios 基础封装
+import axios from 'axios'
+
+const httpInstance = axios.create({
+    baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+    timeout: 5000,
+})
+
+//拦截器
+
+//axios请求拦截器
+httpInstance.interceptors.request.use(config=> {
+    // 在发送请求之前做些什么
+    return config;
+  }, e=> 
+    // 对请求错误做些什么
+    Promise.reject(e));
+//axios 响应式拦截器
+httpInstance.interceptors.response.use(res=> res.data, e=>{
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return Promise.reject(e);
+  });
+export default httpInstance
