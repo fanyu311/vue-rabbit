1e8e49c1a17039f59bfafdd99fc58331f585b702
fanyu311 <116265245+fanyu311@users.noreply.github.com>
2023-06-05 14:53:41 +0200

    layout

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,11 @@
 <template>
   <!-- 一级路由出口组件 -->
   <RouterView/>
-  <div class="test">
-    text scss
-  </div>
+ 
 </template>
 
 <style scoped lang="scss">
-.test {
-  color: $priceColor;
-}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,20 @@
+<script setup>
+
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+
+</script>
+
 <template>
-    <div>
-        我是首页
-        <!-- 二级路由出口 -->
-        <RouterView/> 
-    </div>
+      <!-- 二级路由出口 -->
+     <LayoutNav />
+    <LayoutHeader />
+    <RouterView/> 
+    <LayoutFooter />
+      
+ 
+
 
 </template> 
+
