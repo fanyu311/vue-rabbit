14b163818e7a8af00ffda1c95362410dc4c900e6
fanyu311 <116265245+fanyu311@users.noreply.github.com>
2023-06-05 15:55:13 +0200

    scroll

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,15 +3,18 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
 
 </script>
 
 <template>
       <!-- 二级路由出口 -->
+      <LayoutFixed />
      <LayoutNav />
     <LayoutHeader />
     <RouterView/> 
     <LayoutFooter />
+    
       
  
 
