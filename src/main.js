f8e9b9c9fee893a9c218381b41836c8d81315af9
fanyu311 <116265245+fanyu311@users.noreply.github.com>
2023-06-06 10:09:36 +0200

    panel

--- src/main.js
+++ src/main.js
@@ -20,3 +20,12 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令
+app.directive('img-lazy', {
+    mounted(el, binding){
+        // el: 指令绑定的那个元素 img
+        // binding： binding.value 指令等于号后面绑定的表达式的值 图片url
+        console.log(el, binding.value);
+   }
+})
