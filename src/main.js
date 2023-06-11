252379b2c2291d539c0ed7aa25221f89d4789603
fanyu311 <116265245+fanyu311@users.noreply.github.com>
2023-06-11 18:13:02 +0200

    quanju

--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
 
-// 懒加载指令插件并且注册
+// 引入懒加载指令插件并且注册
 import { lazyPlugin } from '@/directives'
-
-
+//引入全局组件插件
+import { componentPlugin } from '@/components'
 
 // //测试接口函数
 // import { getCategory } from '@/apis/testAPI'
@@ -25,5 +25,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
 
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,6 +1,5 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
-import ImageView from '@/components/ImageView/index.vue'
 import { getDetail} from '@/apis/detail'
 import { onMounted, ref} from 'vue'
 import { useRoute} from 'vue-router'
@@ -12,6 +11,11 @@ const getGoods = async () => {
     goods.value = res.result
 }
 onMounted(() => getGoods())
+
+// sku规格被操作时
+const skuChange = (sku) => {
+  console.log(sku)
+}
 </script>
 
 <template>
@@ -38,7 +42,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView :image-list="goods.mainPictures"/>
+              <XtxImageView :image-list="goods.mainPictures"/>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
@@ -87,7 +91,7 @@ onMounted(() => getGoods())
                 </dl>
               </div>
               <!-- sku组件 -->
-
+              <XtxSku :goods="goods" @change="skuChange"/>
               <!-- 数据组件 -->
 
               <!-- 按钮组件 -->
